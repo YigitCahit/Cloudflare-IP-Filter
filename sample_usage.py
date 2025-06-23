8bf5fe98b0ef52218bed3d9a30cc22dfc2278d24
Mert Cahit <yigitmertcahit@gmail.com>
2025-06-23 14:58:53 +0300

    Update sample_usage.py
    Açıklamalar eklendi

--- sample_usage.py
+++ sample_usage.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from cloudflare_ip_filter import fetch_cloudflare_ips, restrict_to_cloudflare
+from cloudflare_ip_filter import fetch_cloudflare_ips, restrict_to_cloudflare # cloudflare_ip_filter kütüphanesini dahil eder
 
 app = Flask(__name__)
-fetch_cloudflare_ips()
+fetch_cloudflare_ips() # Cloudflare resmi sitesinden IP adreslerini çeker
 
-@app.before_request
+@app.before_request # Her gelen istekten önce çalışacak fonksiyon
 def check_ip():
-    restrict_to_cloudflare()
+    restrict_to_cloudflare(True) # İsteği gönderen IP adresinin, Cloudflare IP listesinde bulunup bulunmadığı kontrol eder
 
 @app.route('/')
 def index():
